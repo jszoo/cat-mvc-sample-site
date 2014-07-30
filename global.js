5181bee1eb1fbe934e2ef92c2ff57c523ee760ba
Ronglin-Kooboo <ronglin@kooboo.com>
2014-07-30 15:10:27 +0800

    use new api of create mvc app

--- global.js
+++ global.js
@@ -4,7 +4,7 @@
 * create date: 2014.6.23
 */
 var mvc = require('cat-mvc');
-var app = mvc.gainApp({ appPath: __dirname });
+var app = mvc({ appPath: __dirname });
 
 // log
 var logger = require('morgan');
